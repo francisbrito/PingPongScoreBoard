1435dfdcaac4f61c4c890f069fbd8303194fc094
Francis Brito <fr.br94@gmail.com>
2014-03-13 13:57:36 -0400

    Got JUnit to work. Added JUnit dependency. Addes some tests for Game class.

--- test/GameTest.java
+++ test/GameTest.java
@@ -1,15 +1,58 @@
-package test;
-
+import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.lang.IllegalStateException;
 
 public class GameTest {
-    @Test
-    public void testGameStateDefaultsToNotSet() {
-        Game game = new Game();
+    private Game game;
 
-        assertEquals("Game.state should default to GameState.NotSet",
-                     GameState.NotSet, game.getState());
+    @Before
+    public void setUp() {
+        this.game = new Game();
+    }
+
+    @Test
+    public void testStateDefaultsToNotSet() {
+        assertEquals("Game#state should default to GameState.NotSet"
+                     , GameState.NotSet
+                     , this.game.getState());
+    }
+
+    @Test
+    public void testTeamsDefaultToTwo() {
+        Team[] teams = this.game.getTeams();
+
+        assertEquals("Game#teams should default to two teams."
+                     , 2
+                     , teams.length);
+    }
+
+    @Test(expected=IllegalStateException.class)
+    public void testStartThrowsIfSetsArentSetupFirst() {
+        this.game.start();
+    }
+
+    @Test
+    public void testStartSetsCurrentSetToFirstSet() {
+        this.game.setNumberOfSets(3);
+
+        Set firstSet = this.game.getSets()[0];
+
+        assertEquals("Game#start should set current set to first set."
+                     , firstSet
+                     , this.game.getCurrentSet());
+    }
+
+    @Test
+    public void testStartSetsStateToStarted() {
+        this.game.setNumberOfSets(3);
+        this.game.start();
+        
+        GameState state = this.game.getState();
+
+        assertEquals("Game#start should set state to started."
+                     , GameState.Started
+                     , state);
     }
 }
