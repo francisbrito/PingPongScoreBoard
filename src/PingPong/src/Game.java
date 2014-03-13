6d67e5850d41ab909f4f69235a64b43c6224bf17
xxkmcxx <kmartinez0890@gmail.com>
2014-03-12 23:45:40 -0400

    game class
    Added game class methods to source

---
+++ src/PingPong/src/Game.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kevin
+ */
+public class Game {
+    
+    private Enum state;
+    private int currentSet ;
+    
+    public Game()
+    {
+        this.currentSet = 0;
+    }
+    
+    public void Start()
+    {
+    }
+    
+    public Enum getState()
+    {
+        return state;
+    }
+    public Set getCurrentSet()
+    {
+        return 
+    }
+    
+    public Set[] getSets()
+    {
+        
+    }
+    public void setNumberOfSets(int sets)
+    {
+        
+    }
+    public void goToSet()
+    {
+        
+    }
+    public void goToPastSet()
+    {
+        
+    }
+    public void goToNextSet()
+    {
+        
+    }
+    public void goToFirstSet()
+    {
+        
+    }
+    public void goToLastSet()
+    {
+        
+    }
+    public Team getTeams()
+    {
+        
+    }
+    public void setNumberOfTeams()
+    {
+        
+    }        
+    public void finish()
+    {
+    }        
+}
--- src/PingPong/src/Team.java
+++ src/PingPong/src/Team.java
@@ -9,4 +9,12 @@
  */
 public class Team {
     
+    private int number;
+    private int score;
+    
+    public Team(int playerCount)
+    {
+       
+    }
+    
 }
